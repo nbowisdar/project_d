9b94c9c3bb7b17ee8b7db523e885d1d3f3164fa1
Username <danya221299@gmail.com>
2022-12-01 22:13:40 -0200

    del auth via Steam, add time in filter

--- csgofloat/api_csgofloat.py
+++ csgofloat/api_csgofloat.py
@@ -18,35 +18,9 @@ class CSGOfloatApi(BaseClass):
             browser_executable_path=uc.find_chrome_executable()
         )
 
-    def __prepare_steam(self):
-        if self.click_element('//img[@src="assets/login-steam.png"]', wait=5):
-            # open steam new tab
-            time.sleep(random.uniform(.2, .58))
-            self.DRIVER.tab_new("https://store.steampowered.com/")
-
-            # switch new tab
-            self.DRIVER.switch_to.window(self.DRIVER.window_handles[-1])
-            time.sleep(3 * random.uniform(.2, .58))
-
-            # go to steam's profile
-            self.click_element('//div[@id="global_header"]//a[@data-tooltip-content=".submenu_username"]')
-            self.xpath_exists('//div[@id="global_header"]//a[@data-tooltip-content=".submenu_username"]')
-            self.DRIVER.reconnect(10 * random.uniform(.2, .58))
-
-            # close and switch tabs
-            self.DRIVER.close()
-            self.DRIVER.switch_to.window(self.DRIVER.window_handles[0])
-
-            # reboot webpage
-            self.DRIVER.refresh()
-            self.DRIVER.reconnect(10 * random.uniform(.2, .58))
-            # auth through steam
-            self.click_element('//input[@id="imageLogin"]')
-
     def auth_csgofloat(self):
         self.DRIVER.get('https://csgofloat.com/')
 
-        self.__prepare_steam()
         time.sleep(6 * random.uniform(.2, .58))
 
         self.DRIVER.get('https://csgofloat.com/db')
@@ -60,15 +34,19 @@ class CSGOfloatApi(BaseClass):
         self.click_element('//button[@mattooltip="Clear Search Parameters"]')
 
         self.send_text_by_elem('//input[@formcontrolname="paintSeed"]', item["paint_seed"])
+        time.sleep(3 * random.uniform(.2, .58))
 
         self.__send_float_value(item["float_value"])
+        time.sleep(2 * random.uniform(.2, .58))
 
         # fix round "float_value" if exists drag settings for float_value
         if self.xpath_exists('//nouislider'):
             self.__send_float_value(item["float_value"])
+            time.sleep(2 * random.uniform(.2, .58))
 
         if item["name"] != "-":
             self.send_text_by_elem('//input[@formcontrolname="name"]', item["name"])
+            time.sleep(3 * random.uniform(.2, .58))
 
         self.click_element('//mat-spinner-button/button')
 
