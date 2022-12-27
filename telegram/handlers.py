5513c196e014d8e343a956062b7a57a3f9ae8797
nbowisdar <380992566619v@gmail.com>
2022-12-27 23:54:24 +0200

    change_bot_a_little

--- telegram/handlers.py
+++ telegram/handlers.py
@@ -7,23 +7,27 @@ from telegram.keyboards import stop_btn, main_kb
 import asyncio
 
 main_router = Router()
-IS_WORKING = False
+#IS_WORKING = False
 
-
-@main_router.message(Text(text='Старт'))
 @main_router.message(Command(commands="start"))
-async def test(message: Message):
-    global IS_WORKING
-    if not IS_WORKING:
-        IS_WORKING = True
-        await message.answer("Бот запущен!", reply_markup=stop_btn)
-        #await pulling_messages(message)
-    else:
-        await message.reply("Кто-то уже запустил бота!")
+async def start(message: Message):
+    await message.answer("Привет, у этого бота нет команд, он запускаеться автоматически вместе со скриптом!")
 
 
-@main_router.message(Text(text="Стоп"))
-async def stop_pulling(message: Message):
-    global IS_WORKING
-    IS_WORKING = False
-    await message.reply("Бот остановлен!", reply_markup=main_kb)
+# @main_router.message(Text(text='Старт'))
+# @main_router.message(Command(commands="start"))
+# async def test(message: Message):
+#     global IS_WORKING
+#     if not IS_WORKING:
+#         IS_WORKING = True
+#         await message.answer("Бот запущен!", reply_markup=stop_btn)
+#         #await pulling_messages(message)
+#     else:
+#         await message.reply("Кто-то уже запустил бота!")
+#
+#
+# @main_router.message(Text(text="Стоп"))
+# async def stop_pulling(message: Message):
+#     global IS_WORKING
+#     IS_WORKING = False
+#     await message.reply("Бот остановлен!", reply_markup=main_kb)
